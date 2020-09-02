7cd9e3ab1c9e323a7ad93b651fdae3da6fe141f4
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2020-09-03 03:10:32 +0700

    pertama

---
+++ database/migrations/2020_08_30_114718_create_authors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('date_of_birth');
+            $table->string('place_of_birth');
+            $table->string('gender');
+            $table->string('email');
+            $table->string('hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
